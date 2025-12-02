3e05c7b11c4895898a8d71c2a63c801a24ea7dfe
pawel78 <pawel.orzechowski@gmail.com>
2025-12-02 06:16:56 -0800

    bug fix: slowed sleep time on the ahrs loop

--- src/ahrs/ahrs.cpp
+++ src/ahrs/ahrs.cpp
@@ -131,7 +131,7 @@ void Ahrs::ahrs_thread_func(Ahrs *ahrs)
         }
         
         // Simulate some processing delay
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
     std::cout << "AHRS thread exiting.\n";
